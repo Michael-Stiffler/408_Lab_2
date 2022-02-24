d5c7531dacf7b09caf2bdd9fab6ac0b14150b1fc
vilqo <56851676+vilqo@users.noreply.github.com>
2022-02-24 15:26:57 -0500

    updates
    updates

--- scripts/Home.js
+++ scripts/Home.js
@@ -1,18 +1,13 @@
-var slideIndex = 0;
+var index = 0;
         
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
+function nextSlide() {
+    var listOfSlideElements = document.getElementsByClassName("slides");
+    for (var i = 0; i < 3; i++) {
+        listOfSlideElements[i].style.display = "none";  
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    index += 1;
+    if (index > 3) {
+        index = 1
+    }    
+    listOfSlideElements[index-1].style.display = "block";  
 }
